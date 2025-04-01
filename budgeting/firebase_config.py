84ce794aa7027d58c4be9facd64e798361712241
William Downing <will.downing00@gmail.com>
2025-04-01 14:12:26 -0400

    Added Edit and Delete functionality to transaction

--- budgeting/firebase_config.py
+++ budgeting/firebase_config.py
@@ -15,6 +15,21 @@ def save_transaction_to_firestore(transaction):
     """Save a transaction to Firestore"""
 
     data = {
+        "name": transaction.name,
+        "category": transaction.category,
+        "amount": float(transaction.amount),
+        "transaction_type": transaction.transaction_type,
+        "created_at": transaction.created_at.isoformat(),
+    }
+    doc_ref = db.collection("transactions").document()  # Let Firestore generate the ID
+    doc_ref.set(data)
+    transaction.id = doc_ref.id  # Save the Firestore-generated ID back to the transaction
+    transaction.save()
+
+def update_transaction_in_firestore(transaction):
+    """Update a transaction in Firestore"""
+
+    data = {
         "name" : transaction.name,
         "category" : transaction.category,
         "amount" : float(transaction.amount),
@@ -22,4 +37,13 @@ def save_transaction_to_firestore(transaction):
         "created_at" : transaction.created_at.isoformat(),
     }
     doc_ref = db.collection("transactions").document(transaction.id)
-    doc_ref.set(data)
+    doc_ref.update(data)
+
+def delete_transaction_in_firestore(transaction):
+    """Delete a transaction in Firestore"""
+    try:
+        doc_ref = db.collection("transactions").document(transaction.id)  # Use transaction.id directly
+        doc_ref.delete()
+        print(f"Transaction {transaction.id} deleted from Firestore.")
+    except Exception as e:
+        print(f"Error deleting transaction {transaction.id} from Firestore: {e}")
